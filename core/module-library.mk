0bedd3df147e2588c7cc99fc5d0dfdff69a53ef8
Sebastien Devaux <sebastien.devaux@laposte.net>
2019-06-18 11:50:16 +0200

    #e378280 factorisation of identical files.

--- core/module-library.mk
+++ core/module-library.mk
@@ -1,7 +1,1 @@
-include $(ABSROOT)/core/module-crules-vars.mk
-include $(PRJROOT)/core/module-adarules.mk
-include $(ABSROOT)/core/module-crules.mk
-ifneq ($(INCTESTS),)
-include $(ABSROOT)/core/module-test.mk
-endif
-include $(ABSROOT)/core/module-scripts.mk
+include $(ABSROOT)/core/module-native.mk
