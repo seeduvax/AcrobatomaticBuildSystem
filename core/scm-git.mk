1b860c90cbba308393ccdddbfb93f8df8e82b13c
Sebastien Devaux <sdevaux@laposte.net>
2019-08-21 17:23:39 +0200

    #e378280 fixed misstypo

--- core/scm-git.mk
+++ core/scm-git.mk
@@ -27,4 +27,4 @@ define abs_scm_branch
  printf '1,$$s/VERSION=.*\$$/VERSION=$(NEW_BRANCH).0/g\n1,$$s/VPARENT=.*$$/VPARENT=$(TAG_VERSION)/g\n1,$$s/VISSUE=.*$$/VISSUE=$(I)/g\nwq\n' | ed app.cfg && \
  git commit app.cfg -m "#$(I) [open branch $(NEW_BRANCH) from $(TAG_VERSION)] $(M)" && \
  git push --all $(GIT_REPOSITORY)
-endif
+endef
