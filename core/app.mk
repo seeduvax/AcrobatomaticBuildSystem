0a01bb478f1e93b499986ad33e32ac4f28dec1da
Sebastien Devaux <sebastien.devaux@laposte.net>
2020-03-02 17:43:16 +0100

    #8b4d566 add traceability to abs version in generated import.mk

--- core/app.mk
+++ core/app.mk
@@ -184,6 +184,7 @@ dist/$(APPNAME)-$(VERSION)/import.mk:
 	@test -f export.mk && \
 	m4 -D__app__=$(APPNAME) -D__version__=$(VERSION) export.mk -D__uselib__="$(USELIB)" > $$PWD/dist/$(APPNAME)-$(VERSION)/import.mk || \
 	printf '\n-include $$(dir $$(lastword $$(MAKEFILE_LIST)))/.abs/index.mk\n$$(eval $$(call extlib_import_template,$(APPNAME),$(VERSION),$(USELIB)))\n$(_extra_import_defs_)\n\n' > $@
+	@echo "# generated: ABS-$(__ABS_VERSION__) $(USER)@"`hostname`" "`date --rfc-3339 s` >> $@
 	@if [ -x extradist.sh ]; then VERSION=$(VERSION) APP=$(APPNAME) APPNAME=$(APPNAME) ./extradist.sh `dirname $@`; fi
 	@mkdir -p dist/$(APPNAME)-$(VERSION)/include/$(APPNAME)
 	@for headerdir in $(patsubst %,%/include,$(filter-out $(NODISTMOD),$(EXPMOD))); \
