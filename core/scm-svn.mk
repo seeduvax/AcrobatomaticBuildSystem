1739f33a8c16ee98292f2a5768de815379e94a91
Sebastien Devaux <sebastien.devaux@laposte.net>
2020-03-04 22:40:03 +0100

    #e1d18d9 added rules to generate release index for svn controlled prj

--- core/scm-svn.mk
+++ core/scm-svn.mk
@@ -5,10 +5,16 @@ SVNROOT:=$(shell LANG=C svn info | grep "^Repository Root:" | sed -e 's/^Reposit
 # SVNPRJROOT is the directory where are the tags, branches and trunk directories
 SVNPRJROOT?=$(SVNROOT)
 SVNURL:=$(shell LANG=C svn info | grep "^URL:" | sed -e 's/^URL: //g')
+SVNURLFROM=$(SVNROOT)/tags/$(APPNAME)/$(APPNAME)-$(VPARENT)
+SVNURLTO=$(SVNROOT)/tags/$(APPNAME)/$(APPNAME)-$(VERSION)
 
 # backward compability.
 TAG_REVISION?=$(SVN_REVISION)
 TAG_REVISION?=HEAD
+
+ifneq ($(SVN_PASSWORD),)
+SVNFLAGS+=--password '$(SVN_PASSWORD)'
+endif
 
 define abs_scm_tag
 @svn copy $(SVNFLAGS) $(SVNURL)@$(TAG_REVISION) $(SVNPRJROOT)/tags/$(APPNAME)/$(APPNAME)-$(TAG_VERSION) -m "$(VISSUE) $(M)"
@@ -27,3 +33,27 @@ define abs_scm_branch
  cd - && \
  rm -rf tmp.$(APPNAME)-$(NEW_BRANCH)
 endef
+
+$(BUILDROOT)/scm/file-list.xml:
+	@$(ABS_PRINT_info) "Generating file index for $(APPNAME)-$(VERSION)"
+	@mkdir -p $(@D)
+	@svn $(SVNFLAGS) ls -R --xml $(SVNURLTO) > $@
+
+$(BUILDROOT)/scm/diff.xml:
+	@$(ABS_PRINT_info) "Generating diff index for $(APPNAME) from $(VPARENT) to-$(VERSION)"
+	@mkdir -p $(@D)
+	@svn $(SVNFLAGS) diff --xml --summarize --notice-ancestry --old=$(SVNURLFROM) --new=$(SVNURLTO) | sed -e "s!$(SVNURLFROM)/!!g" > $@
+
+$(BUILDROOT)/scm/log.xml:
+	@$(ABS_PRINT_info) "Generating log index for $(APPNAME) from $(VPARENT) to-$(VERSION)"
+	@mkdir -p $(@D)
+	@svn $(SVNFLAGS) log --xml $(SVNURLTO) -v -r $(SVNFROMLREV):HEAD > $@
+
+$(BUILDROOT)/scm/checksum.txt:
+	@$(ABS_PRINT_info) "Generating checksum index for $(APPNAME)-$(VERSION)"
+	@rm -rf __tmp_$(APPNAME)
+	@svn $(SVNFLAGS) checkout $(SVNURLTO) __tmp_$(APPNAME)
+	@find __tmp_$(APPNAME) -type f | xargs sha256sum | sed -e 's!__tmp_$(APPNAME)/!!g' > $@
+	@rm -rf __tmp_$(APPNAME)
+
+scm-release:: $(patsubst %,$(BUILDROOT)/scm/%, file-list.xml diff.xml log.xml checksum.txt)
