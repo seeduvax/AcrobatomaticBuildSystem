3c4f3adb612601482858b879f4abdb387a4795bf
Sebastien Devaux <sdevaux@ummon.eduvax>
2020-03-18 16:53:49 +0100

    #1aeaf12 Debugging branch creation with git workspace fixed new branch naming fixed regex to update app.cfg's VERSION

--- core/scm-git.mk
+++ core/scm-git.mk
@@ -26,8 +26,8 @@ define abs_scm_commit
 endef
 
 define abs_scm_branch
-@git checkout -b $(NEW_BRANCH) && \
- printf '1,$$s/VERSION=.*\$$/VERSION=$(NEW_BRANCH).0/g\n1,$$s/VPARENT=.*$$/VPARENT=$(TAG_VERSION)/g\n1,$$s/VISSUE=.*$$/VISSUE=$(I)/g\nwq\n' | ed app.cfg && \
+git checkout -b $(APPNAME)-$(NEW_BRANCH) && \
+ printf '1,$$s/VERSION=.*$$/VERSION=$(NEW_BRANCH).0/g\n1,$$s/VPARENT=.*$$/VPARENT=$(TAG_VERSION)/g\n1,$$s/VISSUE=.*$$/VISSUE=$(I)/g\nwq\n' | ed app.cfg && \
  git commit app.cfg -m "#$(I) [open branch $(NEW_BRANCH) from $(TAG_VERSION)] $(M)" && \
  git push --all $(GIT_REPOSITORY)
 endef
