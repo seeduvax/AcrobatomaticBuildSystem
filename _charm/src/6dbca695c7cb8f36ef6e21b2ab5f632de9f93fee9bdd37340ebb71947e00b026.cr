f97144799c277057d6170dea368ede2fbf24173a
Sebastien Devaux <sebastien.devaux@laposte.net>
2020-07-05 18:21:20 +0200

    #b895f70 issue management

---
+++ _charm/src/6dbca695c7cb8f36ef6e21b2ab5f632de9f93fee9bdd37340ebb71947e00b026.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="6dbca695c7cb8f36ef6e21b2ab5f632de9f93fee9bdd37340ebb71947e00b026" state="open">
+<title>plop</title>
+<reporter>sdevaux</reporter>
+<creation>2020-07-05 18:21:20+02:00</creation>
+<description></description>
+<links>
+<link name="parent">b895f70c84f49aad71079c808f7c87e076866f417d55ec8fcd59b545a3799156</link>
+</links>
+<cf v="null"/>
+</cr>
---
+++ _charm/src/b895f70c84f49aad71079c808f7c87e076866f417d55ec8fcd59b545a3799156.cr
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="b895f70c84f49aad71079c808f7c87e076866f417d55ec8fcd59b545a3799156" state="open">
+<title>Release abs 3.1</title>
+<reporter>sdevaux</reporter>
+<creation>2020-07-05 18:21:20+02:00</creation>
+<description></description>
+<links>
+<link name="parent"></link>
+<link name="child">6dbca695c7cb8f36ef6e21b2ab5f632de9f93fee9bdd37340ebb71947e00b026</link>
+</links>
+<cf v="null"/>
+</cr>
