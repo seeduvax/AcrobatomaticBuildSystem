5288d94ba23e1c669024957ae897ee5222d4de18
fred <astfred2@yahoo.fr>
2020-09-03 05:20:09 +0000

    issue management

---
+++ _charm/src/041a9720f6bdde1560433251abeaa78e86e4a13403ba10bd3fb8799cc3e6ba49.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="041a9720f6bdde1560433251abeaa78e86e4a13403ba10bd3fb8799cc3e6ba49" state="open">
+<title>Conditionnal USELIB using variable in common.mk not taken in account in good way</title>
+<reporter>fred</reporter>
+<creation>2020-09-03 05:20:09+00:00</creation>
+<description></description>
+<links>
+<link name="parent">b895f70c84f49aad71079c808f7c87e076866f417d55ec8fcd59b545a3799156</link>
+</links>
+<cf v="null"/>
+</cr>
