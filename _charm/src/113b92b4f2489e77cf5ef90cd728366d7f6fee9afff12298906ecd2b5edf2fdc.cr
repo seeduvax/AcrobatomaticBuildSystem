f5e9b785bf3d476f2da7009f52b252e50b402304
Sebastien Devaux <sebastien.devaux@eduvax.net>
2020-12-26 21:48:36 +0100

    issue management

---
+++ _charm/src/113b92b4f2489e77cf5ef90cd728366d7f6fee9afff12298906ecd2b5edf2fdc.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="113b92b4f2489e77cf5ef90cd728366d7f6fee9afff12298906ecd2b5edf2fdc" state="open">
+<title>Remove use of ed command</title>
+<reporter>sdevaux</reporter>
+<creation>2020-12-26 21:48:34+01:00</creation>
+<description></description>
+<links>
+<link name="parent">9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
+</links>
+<cf v="null"/>
+</cr>
