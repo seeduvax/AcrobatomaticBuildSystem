773db67f99769b7d46b0c385f9bc05447b431d0c
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-01-26 17:15:06 +0100

    issue management

--- _charm/src/9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d.cr
+++ _charm/src/9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d.cr
@@ -9,6 +9,7 @@
 <link name="child">113b92b4f2489e77cf5ef90cd728366d7f6fee9afff12298906ecd2b5edf2fdc</link>
 <link name="child">ff4c22d8a4e463af5f67ba25c2b19ccae6c97dbdef5fcd3949679a85bea9067c</link>
 <link name="child">273739c2a03d295d822a721e05036d11c48aba74f360e2c91cda9788d27dd442</link>
+<link name="child">d562593b9886cb8d2bccf1870ac590280da2719c30d3fd3b1d6bc7fcc77af9e0</link>
 </links>
 <cf v="null"/>
 </cr>
---
+++ _charm/src/d562593b9886cb8d2bccf1870ac590280da2719c30d3fd3b1d6bc7fcc77af9e0.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="d562593b9886cb8d2bccf1870ac590280da2719c30d3fd3b1d6bc7fcc77af9e0" state="open">
+<title>whitespace are improperly added into the generated import.mk when applying extra_import_defs cnofiguration</title>
+<reporter>sdevaux</reporter>
+<creation>2021-01-26 17:15:06+01:00</creation>
+<description></description>
+<links>
+<link name="parent">9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
+</links>
+<cf v="null"/>
+</cr>
