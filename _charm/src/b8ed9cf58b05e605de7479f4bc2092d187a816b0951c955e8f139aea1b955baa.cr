4b6f53aa059e637aa2b7ead9513bdcb776502f83
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-02-17 16:50:08 +0100

    #9b2cb58 issue management

---
+++ _charm/src/b8ed9cf58b05e605de7479f4bc2092d187a816b0951c955e8f139aea1b955baa.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="b8ed9cf58b05e605de7479f4bc2092d187a816b0951c955e8f139aea1b955baa" state="open">
+<title>Add code generation handling capability to java modules</title>
+<reporter>sdevaux</reporter>
+<creation>2021-02-17 16:50:08+01:00</creation>
+<description></description>
+<links>
+<link name="parent">9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
+</links>
+<cf v="null"/>
+</cr>
