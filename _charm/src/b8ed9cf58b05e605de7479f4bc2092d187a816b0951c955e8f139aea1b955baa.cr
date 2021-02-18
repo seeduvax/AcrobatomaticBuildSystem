ff07d3b2c55d2c46997e508906ab1895c1a903b7
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-02-18 18:21:30 +0100

    #b8ed9cf use specific dir for jar image build

--- _charm/src/b8ed9cf58b05e605de7479f4bc2092d187a816b0951c955e8f139aea1b955baa.cr
+++ _charm/src/b8ed9cf58b05e605de7479f4bc2092d187a816b0951c955e8f139aea1b955baa.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="b8ed9cf58b05e605de7479f4bc2092d187a816b0951c955e8f139aea1b955baa" state="open">
+<cr id="b8ed9cf58b05e605de7479f4bc2092d187a816b0951c955e8f139aea1b955baa" state="working">
 <title>Add code generation handling capability to java modules</title>
 <reporter>sdevaux</reporter>
 <creation>2021-02-17 16:50:08+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
 </links>
-<cf v="null"/>
+<cf v="sdevaux 2021-02-18T18:22:15+01:00"/>
 </cr>
--- sampleprj/jmod/src/fr/example/sampleprj/jmod/Main.java
+++ sampleprj/jmod/src/fr/example/sampleprj/jmod/Main.java
@@ -1,4 +1,6 @@
-package fr.example.BuildScriptTester.jmod;
+package fr.example.sampleprj.jmod;
+
+import res.Const;
 
 /**
  *
@@ -9,5 +11,6 @@ public class Main {
     } 
     public static void main(String[] args) {
         System.out.println("Java runs !");
+        System.out.println("Version="+Const.version);
     }
 }
