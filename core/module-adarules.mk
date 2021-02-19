cacfc032c656dc6f60480ae47067d87697d0f8e5
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-02-19 14:45:59 +0100

    #2624922 rm debug trace, support adb in obj dir

--- core/module-adarules.mk
+++ core/module-adarules.mk
@@ -8,7 +8,14 @@ OBJS+=$(ADAOBJS)
 
 # ada files compilation
 $(OBJDIR)/%.o: src/%.adb
-	@$(ABS_PRINT_info) "Compiling  KKK $< ..."
+	@$(ABS_PRINT_info) "Compiling ada $< ..."
+	@mkdir -p $(@D)
+	@echo `date --rfc-3339 s`"> $(ADAC) $(ADAFLAGS) -c $< -D $(@D)" >> $(TRDIR)/build.log
+	@$(ADAC) $(ADAFLAGS) -c $< -o $@ || ( $(ABS_PRINT_error) "Failed: ADAFLAGS=$(ADAFLAGS)" ; exit 1 )
+
+# generated ada files compilation
+$(OBJDIR)/%.o: $(OBJDIR)/%.adb
+	@$(ABS_PRINT_info) "Compiling generated ada $< ..."
 	@mkdir -p $(@D)
 	@echo `date --rfc-3339 s`"> $(ADAC) $(ADAFLAGS) -c $< -D $(@D)" >> $(TRDIR)/build.log
 	@$(ADAC) $(ADAFLAGS) -c $< -o $@ || ( $(ABS_PRINT_error) "Failed: ADAFLAGS=$(ADAFLAGS)" ; exit 1 )
