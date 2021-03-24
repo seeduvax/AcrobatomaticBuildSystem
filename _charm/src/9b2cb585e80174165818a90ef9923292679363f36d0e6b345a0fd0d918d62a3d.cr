fb789f6904c181eac60ebc3eae688e33dbdb9239
fred <astfred2@yahoo.fr>
2021-03-24 23:21:14 +0000

    #9b2cb58 issue management

--- _charm/src/9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d.cr
+++ _charm/src/9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d.cr
@@ -15,6 +15,7 @@
 <link name="child">32ae373d2a2412c59f695bd7f107273692bbc545bfc4c14b85e80f5cfd636b93</link>
 <link name="child">76680dcc93f70d049a1be0c4b539f1bd1a7bc90ed1ae5754b2dc718e71115ccd</link>
 <link name="child">e2f125bbfce65de7c5de98006d05df82883c31f20a5efc5fcb3d435778a33fa2</link>
+<link name="child">25292ece55198d0eea88054717137ac3791d7cb8e06b1d1f532d50e8da870b0b</link>
 </links>
 <cf v="sdevaux 2021-02-17T16:47:55+01:00"/>
 </cr>
