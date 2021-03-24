f54c4ed1d2c8b10e7b48d007d131bcde08451af7
fred <astfred2@yahoo.fr>
2021-03-24 23:25:11 +0000

    #25292ec  #25292ec #25292ec: add task to add sanitizer option

--- _charm/src/25292ece55198d0eea88054717137ac3791d7cb8e06b1d1f532d50e8da870b0b.cr
+++ _charm/src/25292ece55198d0eea88054717137ac3791d7cb8e06b1d1f532d50e8da870b0b.cr
@@ -1,11 +1,10 @@
-<?xml version="1.0" encoding="utf-8"?>
-<cr id="25292ece55198d0eea88054717137ac3791d7cb8e06b1d1f532d50e8da870b0b" state="open">
-<title>Add option to compile cpp using sanitizers</title>
-<reporter>fred</reporter>
-<creation>2021-03-24 23:21:14+00:00</creation>
-<description></description>
-<links>
-<link name="parent">9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
-</links>
-<cf v="null"/>
+<?xml version="1.0" encoding="UTF-8"?><cr id="25292ece55198d0eea88054717137ac3791d7cb8e06b1d1f532d50e8da870b0b" state="working">
+    <title>Add option to compile cpp using sanitizers</title>
+    <reporter>fred</reporter>
+    <creation>2021-03-24 23:21:14+00:00</creation>
+    <description/>
+    <links>
+        <link name="parent"> 9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
+    </links>
+    <cf v="null"> </cf>
 </cr>
