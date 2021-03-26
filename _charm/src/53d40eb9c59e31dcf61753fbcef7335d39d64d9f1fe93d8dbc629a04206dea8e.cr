9c31e6b15330f598f8106b3819e6abfeb7f307aa
fred <astfred2@yahoo.fr>
2021-03-26 21:52:13 +0000

    #53d40eb Remove : from commit message

--- _charm/src/53d40eb9c59e31dcf61753fbcef7335d39d64d9f1fe93d8dbc629a04206dea8e.cr
+++ _charm/src/53d40eb9c59e31dcf61753fbcef7335d39d64d9f1fe93d8dbc629a04206dea8e.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="53d40eb9c59e31dcf61753fbcef7335d39d64d9f1fe93d8dbc629a04206dea8e" state="resolved">
+<cr id="53d40eb9c59e31dcf61753fbcef7335d39d64d9f1fe93d8dbc629a04206dea8e" state="working">
 <title>Improve the prepare commit message for charm issue</title>
 <reporter>fred</reporter>
 <creation>2021-03-26 21:35:43+00:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
 </links>
-<cf v="fred 2021-03-26T21:36:19+00:00"/>
+<cf v="fred 2021-03-26T21:52:13+00:00"/>
 </cr>
