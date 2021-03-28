8d1a7503cbd5b726518113ce855ec89ad4d7b6fd
fred <astfred2@yahoo.fr>
2021-03-28 22:03:24 +0000

    #9b2cb58 Add missing resolution of issues

--- _charm/src/25292ece55198d0eea88054717137ac3791d7cb8e06b1d1f532d50e8da870b0b.cr
+++ _charm/src/25292ece55198d0eea88054717137ac3791d7cb8e06b1d1f532d50e8da870b0b.cr
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?><cr id="25292ece55198d0eea88054717137ac3791d7cb8e06b1d1f532d50e8da870b0b" state="resolved">
+<?xml version="1.0" encoding="UTF-8"?><cr id="25292ece55198d0eea88054717137ac3791d7cb8e06b1d1f532d50e8da870b0b" state="closed" delivered="abs-3.2.4">
     <title>Add option to compile cpp using sanitizers</title>
     <reporter>fred</reporter>
     <creation>2021-03-24 23:21:14+00:00</creation>
