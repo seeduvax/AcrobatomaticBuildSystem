daab847d368c41b2abbc82e1facc6c49daff5287
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-04-02 08:56:19 +0200

    #e2f125b remove debug traces

--- doc/pumldeps.sh
+++ doc/pumldeps.sh
@@ -6,10 +6,8 @@ then
 fi
 
 catdeps() {
-echo "DDDDD: " "$@" >&2
 fgrep "$@" module.cfg | sed -e 's/:.*@/ /g' | while read srcFile startuml img endofline
 do
-echo "DDD: " "$srcFile $startuml $img / $endofline" >&2
 	cat << EOF
 \$(HTMLDIR)/$img: \$(patsubst src/%,\$(OBJDIR)/%.pumlgenerated,$srcFile)
 
