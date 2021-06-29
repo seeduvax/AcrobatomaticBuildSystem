b939c79feff576931a3406b15ef516f32617aee5
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-06-29 08:54:06 +0200

    #9b2cb58 issue management

---
+++ _charm/src/62783de6ce8d19757a16bf891842e1d35a1a709d85873d3f166382bf18335e36.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="62783de6ce8d19757a16bf891842e1d35a1a709d85873d3f166382bf18335e36" state="open">
+<title>Release abs 3.3</title>
+<reporter>sdevaux</reporter>
+<creation>2021-06-29 08:54:06+02:00</creation>
+<description></description>
+<links>
+<link name="parent">9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
+</links>
+<cf v="null"/>
+</cr>
