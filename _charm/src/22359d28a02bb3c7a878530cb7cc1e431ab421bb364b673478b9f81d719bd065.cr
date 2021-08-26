0ae60081d549099e426f491799c6913e5a2f5461
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-08-26 14:00:35 +0200

    #bfca44f issue management

---
+++ _charm/src/22359d28a02bb3c7a878530cb7cc1e431ab421bb364b673478b9f81d719bd065.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="22359d28a02bb3c7a878530cb7cc1e431ab421bb364b673478b9f81d719bd065" state="open">
+<title>cppunit library no longer builds.</title>
+<reporter>sdevaux</reporter>
+<creation>2021-08-26 14:00:35+02:00</creation>
+<description></description>
+<links>
+<link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
+</links>
+<cf v="null"/>
+</cr>
--- _charm/src/bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e.cr
+++ _charm/src/bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e.cr
@@ -10,6 +10,7 @@
 <links>
 <link name="parent"></link>
 <link name="child">fa5acdbcce074748469b9492c43261d0d999e5182aab805a897df3398d826f98</link>
+<link name="child">22359d28a02bb3c7a878530cb7cc1e431ab421bb364b673478b9f81d719bd065</link>
 </links>
 <cf v="m026258 2021-08-06T13:12:45+02:00"/>
 </cr>
