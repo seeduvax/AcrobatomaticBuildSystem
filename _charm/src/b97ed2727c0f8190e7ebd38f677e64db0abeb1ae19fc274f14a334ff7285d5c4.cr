a8750a5179e4dee5f419deda1d716f7c668aef9d
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-08-26 14:03:55 +0200

    #bfca44f issue management

---
+++ _charm/src/b97ed2727c0f8190e7ebd38f677e64db0abeb1ae19fc274f14a334ff7285d5c4.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="b97ed2727c0f8190e7ebd38f677e64db0abeb1ae19fc274f14a334ff7285d5c4" state="open">
+<title>add lua abs packager.</title>
+<reporter>sdevaux</reporter>
+<creation>2021-08-26 14:03:55+02:00</creation>
+<description></description>
+<links>
+<link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
+</links>
+<cf v="null"/>
+</cr>
