48493a261ad5d627707683b2d5d329b06a4ca4e2
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-08-27 12:51:59 +0200

    #bfca44f issue management

--- _charm/src/bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e.cr
+++ _charm/src/bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e.cr
@@ -12,6 +12,7 @@
 <link name="child">fa5acdbcce074748469b9492c43261d0d999e5182aab805a897df3398d826f98</link>
 <link name="child">22359d28a02bb3c7a878530cb7cc1e431ab421bb364b673478b9f81d719bd065</link>
 <link name="child">b97ed2727c0f8190e7ebd38f677e64db0abeb1ae19fc274f14a334ff7285d5c4</link>
+<link name="child">a6e15ac38503035282d0ab5750c0ff46597c9831a7d35e1b71ab19b8f034eec2</link>
 </links>
 <cf v="m026258 2021-08-06T13:12:45+02:00"/>
 </cr>
