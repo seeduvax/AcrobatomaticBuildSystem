1768c3cef69ff0608d128cfa38c5b4fc0affaa61
m096258 <frederic.astruc@ariane.group>
2021-09-29 14:33:38 +0200

    #9b2cb58 issue management

---
+++ _charm/src/90b84d9573836d5182a4f73f73ea5b09d8d0f8e34acc39253f366a8a30c70ca9.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="90b84d9573836d5182a4f73f73ea5b09d8d0f8e34acc39253f366a8a30c70ca9" state="open">
+<title>Regression on linux driver compilation when disable src (from</title>
+<reporter>m096258</reporter>
+<creation>2021-09-29 14:33:38+02:00</creation>
+<description></description>
+<links>
+<link name="parent">9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
+</links>
+<cf v="null"/>
+</cr>
--- _charm/src/9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d.cr
+++ _charm/src/9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d.cr
@@ -26,6 +26,7 @@
 <link name="child">939199855cb8793b784c12c2e2bdd3bc300a0a26aaab23b45f4e69ea9ef87325</link>
 <link name="child">b0d4c38f877c417f653548cf7b190d30561cdbf58c362a3a93cd635154842f64</link>
 <link name="child">a90d9ab6527b40843cca414642f617fa206c12352a8939f220617e508b3bbffe</link>
+<link name="child">90b84d9573836d5182a4f73f73ea5b09d8d0f8e34acc39253f366a8a30c70ca9</link>
 </links>
 <cf v="sdevaux 2021-06-29T09:19:45+02:00"/>
 </cr>
