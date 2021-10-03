870fcf387ebc182e308ed738883dffba7175e79a
Sebastien Devaux <sebastien.devaux@eduvax.net>
2021-10-03 15:12:52 +0200

    #aebaf2d set ghdl package search path from USEMOD

--- _charm/src/aebaf2d5e8e82fe17476ba42ec400b0abeffeccf00a89db20cd18b9df40cdf67.cr
+++ _charm/src/aebaf2d5e8e82fe17476ba42ec400b0abeffeccf00a89db20cd18b9df40cdf67.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="aebaf2d5e8e82fe17476ba42ec400b0abeffeccf00a89db20cd18b9df40cdf67" state="open">
+<cr id="aebaf2d5e8e82fe17476ba42ec400b0abeffeccf00a89db20cd18b9df40cdf67" state="working">
 <title>VHDL library handling</title>
 <reporter>sdevaux</reporter>
 <creation>2021-10-03 15:11:18+02:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
 </links>
-<cf v="null"/>
+<cf v="sdevaux 2021-10-03T15:13:29+02:00"/>
 </cr>
