058196fc5da7b75faa51fab5511118dfb936808d
Sebastien Devaux <sebastien.devaux@ariane.group>
2021-10-11 17:07:22 +0200

    issue management

---
+++ _charm/src/4856f497f1848fd167d5c5f6102cdc803689fcee276f51299600ac2175ef8b44.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="4856f497f1848fd167d5c5f6102cdc803689fcee276f51299600ac2175ef8b44" state="open">
+<title>TBC/TBD sentances are not properly line wrapped when rendiring heml doc in pdf.</title>
+<reporter>m026258</reporter>
+<creation>2021-10-11 17:07:21+02:00</creation>
+<description></description>
+<links>
+<link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
+</links>
+<cf v="null"/>
+</cr>
