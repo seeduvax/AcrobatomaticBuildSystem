1210758a421575bbb703f59e7726e198a4fbeed8
m096258 <frederic.astruc@ariane.group>
2021-12-02 13:44:35 +0100

    #2d847ef Issue management

--- _charm/src/2d847ef26b9903085695935ce94e1cb664523f69ef267f49fce948942b46286b.cr
+++ _charm/src/2d847ef26b9903085695935ce94e1cb664523f69ef267f49fce948942b46286b.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="2d847ef26b9903085695935ce94e1cb664523f69ef267f49fce948942b46286b" state="working">
+<cr id="2d847ef26b9903085695935ce94e1cb664523f69ef267f49fce948942b46286b" state="working" delivered="abs-3.3.6">
 <title>Tests are executed even if NOTEST is set if another module depends on this module</title>
 <reporter>m096258</reporter>
 <creation>2021-12-02 13:39:54+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">9b2cb585e80174165818a90ef9923292679363f36d0e6b345a0fd0d918d62a3d</link>
 </links>
-<cf v="m096258 2021-12-02T13:41:42+01:00"/>
+<cf v="m096258 2021-12-02T13:44:35+01:00"/>
 </cr>
