926120498b69d976d8e6fa7fef968f3fc955736d
m096258 <frederic.astruc@ariane.group>
2022-01-13 13:55:07 +0100

    #bfca44f Issue Management

--- _charm/src/26a751bc257b1d57795fd267cfb097d55545f163e497f06cabbb2759f3a65358.cr
+++ _charm/src/26a751bc257b1d57795fd267cfb097d55545f163e497f06cabbb2759f3a65358.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="26a751bc257b1d57795fd267cfb097d55545f163e497f06cabbb2759f3a65358" state="working">
+<cr id="26a751bc257b1d57795fd267cfb097d55545f163e497f06cabbb2759f3a65358" state="closed" delivered="abs-3.3.8">
 <title>The editions table contains sentence "-0.5cm" when compiling heml doc on debian9</title>
 <reporter>m096258</reporter>
 <creation>2022-01-13 13:54:00+01:00</creation>
