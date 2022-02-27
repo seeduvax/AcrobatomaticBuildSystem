ab2d639d60e4b8e8b39c23237d4e801618946291
Sebastien Devaux <sebastien.devaux@eduvax.net>
2022-02-27 18:23:15 +0100

    #2d904a8 more robust regex to update CR timestamp

--- _charm/src/2d904a87516423f8ab017dfe3eccee5d86e5f41d2b04b016e9ec9caa2eb1517f.cr
+++ _charm/src/2d904a87516423f8ab017dfe3eccee5d86e5f41d2b04b016e9ec9caa2eb1517f.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="2d904a87516423f8ab017dfe3eccee5d86e5f41d2b04b016e9ec9caa2eb1517f" state="open">
+<cr id="2d904a87516423f8ab017dfe3eccee5d86e5f41d2b04b016e9ec9caa2eb1517f" state="working">
 <title>charm CR commit timestamping does not work after CR edit.</title>
 <reporter>sdevaux</reporter>
 <creation>2022-02-27 18:23:01+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
 </links>
-<cf v="null"/>
+<cf v="sdevaux 2022-02-27T18:23:37+01:00"/>
 </cr>
