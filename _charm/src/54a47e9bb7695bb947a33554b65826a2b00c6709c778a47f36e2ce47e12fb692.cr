b1a5ab7211deddb3a99f9da2a768069eb676c6cc
Sebastien Devaux <sebastien.devaux@eduvax.net>
2022-09-22 14:55:30 +0200

    #54a47e9  #bfca44f issue management

---
+++ _charm/src/54a47e9bb7695bb947a33554b65826a2b00c6709c778a47f36e2ce47e12fb692.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="54a47e9bb7695bb947a33554b65826a2b00c6709c778a47f36e2ce47e12fb692" state="open">
+<title>ABS_PRINT level filtering</title>
+<reporter>sdevaux</reporter>
+<creation>2022-09-22 14:55:30+02:00</creation>
+<description></description>
+<links>
+<link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
+</links>
+<cf v="null"/>
+</cr>
--- _charm/src/bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e.cr
+++ _charm/src/bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e.cr
@@ -42,6 +42,7 @@
 <link name="child">9bc09c631f653ef2205fd61e02dc69c106220d14bba617e7f3d05d583af6c417</link>
 <link name="child">1be57e3c762d6e91c411e090284f46f235ddca0486888fb0a20182b6437d20b7</link>
 <link name="child">34a59d3a1edc9a94e63588fc465f651a2f351401e5f55b3b51c1f7ac37d8de4b</link>
+<link name="child">54a47e9bb7695bb947a33554b65826a2b00c6709c778a47f36e2ce47e12fb692</link>
 </links>
 <cf v="m026258 2022-07-01T15:18:59+02:00"/>
 </cr>
