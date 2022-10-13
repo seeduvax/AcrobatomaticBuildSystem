2ce1edcf46f74f33a118419b5e3905c9fc3fc3ea
Nicolas FRAYSSE <nicolas.fraysse.external@ariane.group>
2022-10-13 09:22:07 +0200

    #bfca44f issue management

---
+++ _charm/src/522e55b538e8e507b6195859cfc2c7bfb490e8d2dd3393320b00a5fb949ac443.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="522e55b538e8e507b6195859cfc2c7bfb490e8d2dd3393320b00a5fb949ac443" state="open">
+<title>Update default Python version to 3 as Python 2 is deprecated since 1/1/2020</title>
+<reporter>ft30997</reporter>
+<creation>2022-10-13 09:22:07+02:00</creation>
+<description></description>
+<links>
+<link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
+</links>
+<cf v="null"/>
+</cr>
