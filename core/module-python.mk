08ca875b97a89e1ba63ec96471d631b50e7f9f3b
Sebastien Devaux <sebastien.devaux@ariane.group>
2022-10-14 18:49:26 +0200

    #522e55b reworked python interpreter selection
    Exit with explicit error when user selected python interpreter is not
    available on the current host.
    + few comments and messages display improvements

--- core/module-python.mk
+++ core/module-python.mk
@@ -4,28 +4,24 @@
 ## 
 ## python services variables:
 ## 
-# python interpreter run command
-# Tested versions: 2.6 
-# Not tested versions: 2.7 and 3.x
-##  - PP: path of python interpreter:
-##  - if PP is already set don't do anything
-##  - if PP is not set, attempt to use python3
-##  - if python3 does not exist, use python 2 and warn the user that no Python 3 interpreter was found
+##  - PP: python interpreter run command, default setting is searching for 
+##        python3 first, then fallback on default host system's python.
+PP?=$(shell which python3 2>/dev/null)
 ifeq ($(PP),)
-PP_3_EXISTS := $(shell /usr/bin/python3 --version 1> /dev/null 2>&1 && echo "true" || echo "false")
-ifeq ($(PP_3_EXISTS),true)
-PP := /usr/bin/python3
+PP:=$(wildcard /usr/bin/python3)
+ifeq ($(PP),)
+PP:=$(shell which python 2>/dev/null)
+endif
+endif
+
+ifeq ($(shell which $(PP) 2>/dev/null),)
+$(info $(shell $(ABS_PRINT_error) "No python interpreter found, python services disabled."))
+ifneq ($(PP),)
+$(info $(shell $(ABS_PRINT_error) "Requested python command was: $(PP)"))
+endif
 else
-PP := /usr/bin/python
-endif
-endif
-
-PYTHON_VERSION:=$(word 2,$(shell $(PP) --version))
-
-ifeq ($(findstring 3.,$(PYTHON_VERSION)),)
-$(warning Could not find a Python 3 interpreter, using Python 2 instead)
-endif
-
+PYTHON_VERSION:=$(word 2,$(shell $(PP) --version 2>&1))
+$(info $(shell $(ABS_PRINT_debug) "Selected python interpreter / version: $(PP) / $(PYTHON_VERSION)"))
 # Initialize these environment variables
 # so that python interpreter can read their values
 # Otherwise ' PYTHONPATH="..."; python ... ' 
@@ -34,12 +30,11 @@ export PYTHONPATH=
 export LD_LIBRARY_PATH=
 
 
-# Python debugger
-# NB: If the installation of additional
-# debuggers is possible, I would suggest to use 
-# 'pydb' that has an enhanced debug interface
-# than the classic 'pdb'
-PDB=pdb
+##  - PDB: Python debugger
+##        default is pdb
+##        pydb is known to be a much improved python debugger, consider its use 
+##        if available from your development environment
+PDB?=pdb
 # python path:
 # - debug/lib/python : 
 #   	localization of the app 
@@ -110,7 +105,7 @@ $(PY_MODDIR)/%.py: $(PY_SRCDIR)/%.py
 	@$(ABS_PRINT_info) "Processing $<..."
 	@mkdir -p $(@D)
 	@cp $< $@
-	cd $(PY_MODDIR) ; $(PP_COMPILE) $@
+	@cd $(PY_MODDIR) ; $(PP_COMPILE) $@
 
 endif
 
@@ -145,4 +140,4 @@ ifneq ($(INCTESTS),)
 endif
 
 include $(ABSROOT)/core/module-scripts.mk
-
+endif
