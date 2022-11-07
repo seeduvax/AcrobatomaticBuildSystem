d5e34fd30fdcbbe78d4d09a1f89a0271031aee79
m025909 <florian.haradji@ariane.group>
2022-11-07 11:57:02 +0100

    #0e2c238 changed issue title

--- _charm/src/0e2c2383f00998301b041553528473de97ce76b908f37e2cb0cf925306483298.cr
+++ _charm/src/0e2c2383f00998301b041553528473de97ce76b908f37e2cb0cf925306483298.cr
@@ -1,11 +1,10 @@
-<?xml version="1.0" encoding="utf-8"?>
-<cr id="0e2c2383f00998301b041553528473de97ce76b908f37e2cb0cf925306483298" state="open">
-<title></title>
+<?xml version="1.0" encoding="UTF-8"?><cr id="0e2c2383f00998301b041553528473de97ce76b908f37e2cb0cf925306483298" state="resolved">
+<title>default to read-only extlib dir</title>
 <reporter>m025909</reporter>
 <creation>2022-11-07 11:52:44+01:00</creation>
-<description></description>
+<description/>
 <links>
-<link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
+<link name="parent"> bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
 </links>
-<cf v="null"/>
+<cf v="null"> </cf>
 </cr>
