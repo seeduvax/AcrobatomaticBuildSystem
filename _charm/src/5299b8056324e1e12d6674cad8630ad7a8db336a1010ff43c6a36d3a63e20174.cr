82129dd28d2fab996991edb81b3ddbc6ea1bcd69
Sebastien Devaux <sebastien.devaux@eduvax.net>
2022-12-02 12:59:54 +0100

    #bfca44f issue management

---
+++ _charm/src/5299b8056324e1e12d6674cad8630ad7a8db336a1010ff43c6a36d3a63e20174.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="5299b8056324e1e12d6674cad8630ad7a8db336a1010ff43c6a36d3a63e20174" state="open">
+<title>Charm post commit hook no more running with freshly created workspace</title>
+<reporter>sdevaux</reporter>
+<creation>2022-12-02 12:59:53+01:00</creation>
+<description></description>
+<links>
+<link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
+</links>
+<cf v="null"/>
+</cr>
