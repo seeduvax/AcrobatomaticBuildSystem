ad468cb4911df54030f6c8e0e0d1cf779fa7eeed
Andre ASTYL <andre.astyl@ariane.group>
2023-01-03 11:47:12 +0100

    #27c9e8b #27c9e8b: to resolved state

--- _charm/src/27c9e8b216b28d5a931f24145a26b8c1da495cbba9a2eaacdf4eac2bb9c2b617.cr
+++ _charm/src/27c9e8b216b28d5a931f24145a26b8c1da495cbba9a2eaacdf4eac2bb9c2b617.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="27c9e8b216b28d5a931f24145a26b8c1da495cbba9a2eaacdf4eac2bb9c2b617" state="working">
+<cr id="27c9e8b216b28d5a931f24145a26b8c1da495cbba9a2eaacdf4eac2bb9c2b617" state="working" delivered="abs-3.3.19" delivered="abs-3.3.19">
 <title>add remote debugtest targets (using gdbserver) + add print target to generate vscode launch.json configuration to remotely gdb attach</title>
 <reporter>aa</reporter>
 <creation>2023-01-03 11:39:15+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
 </links>
-<cf v="aa 2023-01-03T11:42:42+01:00"/>
+<cf v="aa 2023-01-03T11:47:12+01:00"/>
 </cr>
