eaf873dc482f3d977dbf18d6f80a629b67639ca8
Sebastien Devaux <sebastien.devaux@eduvax.net>
2023-01-26 17:55:55 +0100

    #c687dee introducing pattern based dist repo spec

--- _charm/src/c687dee36eb9bed8cfb57363519a12661ab859ab6cc0e72f8f47aa449b462d33.cr
+++ _charm/src/c687dee36eb9bed8cfb57363519a12661ab859ab6cc0e72f8f47aa449b462d33.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="c687dee36eb9bed8cfb57363519a12661ab859ab6cc0e72f8f47aa449b462d33" state="open">
+<cr id="c687dee36eb9bed8cfb57363519a12661ab859ab6cc0e72f8f47aa449b462d33" state="working">
 <title>Enable user define dist repo layout</title>
 <reporter>sdevaux</reporter>
 <creation>2023-01-26 17:55:37+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
 </links>
-<cf v="null"/>
+<cf v="sdevaux 2023-01-26T17:56:28+01:00"/>
 </cr>
