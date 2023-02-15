6df525bba090670dd3ece7a80af2579db22c90dc
Frédéric ASTRUC <astfred2@yahoo.fr>
2023-02-15 11:53:17 +0100

    #a62c68c Change version in app.cfg

--- _charm/src/a62c68c0be9e26db47b59ab6d561138c5cd9ccff83a231e742393892df6b9247.cr
+++ _charm/src/a62c68c0be9e26db47b59ab6d561138c5cd9ccff83a231e742393892df6b9247.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="a62c68c0be9e26db47b59ab6d561138c5cd9ccff83a231e742393892df6b9247" state="open">
+<cr id="a62c68c0be9e26db47b59ab6d561138c5cd9ccff83a231e742393892df6b9247" state="working">
 <title>Release abs 3.4</title>
 <reporter>m096258</reporter>
 <creation>2023-02-15 11:50:55+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
 </links>
-<cf v="null"/>
+<cf v="m096258 2023-02-15T11:53:17+01:00"/>
 </cr>
