088b53d2d64c58fc02ad287f3db51feb41a91fe5
Frédéric ASTRUC <astfred2@yahoo.fr>
2023-02-15 11:50:55 +0100

    #bfca44f issue management

---
+++ _charm/src/a62c68c0be9e26db47b59ab6d561138c5cd9ccff83a231e742393892df6b9247.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="a62c68c0be9e26db47b59ab6d561138c5cd9ccff83a231e742393892df6b9247" state="open">
+<title>Release abs 3.4</title>
+<reporter>m096258</reporter>
+<creation>2023-02-15 11:50:55+01:00</creation>
+<description></description>
+<links>
+<link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
+</links>
+<cf v="null"/>
+</cr>
