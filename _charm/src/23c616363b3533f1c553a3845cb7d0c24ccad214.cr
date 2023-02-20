7166e832ae3c72337dea4fc681235ed2659a93e3
Sebastien Devaux <sebastien.devaux@eduvax.net>
2023-02-20 11:59:35 +0100

    #7bc74cc CR management

--- _charm/src/23c616363b3533f1c553a3845cb7d0c24ccad214.cr
+++
@@ -1,3 +0,0 @@
-<?xml version="1.0" encoding="utf-8"?>
-<cr id="23c616363b3533f1c553a3845cb7d0c24ccad214" state="closed">
-<title>Extract download part into a macro to permit the download of elements from others targets than $(ABS_CACHE)/
--- _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
+++ _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
@@ -8,5 +8,5 @@
 <link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
 <link name="child">734173abecafcd87c04cc39d73ce13f029f7d2f527923ee5bc7e6b9e5bc66a59</link>
 </links>
-<cf v="sdevaux 2023-02-20T11:48:48+01:00"/>
+<cf v="sdevaux 2023-02-20T11:59:54+01:00"/>
 </cr>
