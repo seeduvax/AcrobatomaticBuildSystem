00efd91a4b74005922ba494c3681c1eb81723772
Sebastien Devaux <sebastien.devaux@eduvax.net>
2023-02-24 13:53:41 +0100

    #71f4176 backporting fix from abs-3.4

--- _charm/src/71f4176ab9692bdc39092679646c812b11c8ebc89a93e8b10a39dafed2ca5eb1.cr
+++ _charm/src/71f4176ab9692bdc39092679646c812b11c8ebc89a93e8b10a39dafed2ca5eb1.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="71f4176ab9692bdc39092679646c812b11c8ebc89a93e8b10a39dafed2ca5eb1" state="open">
+<cr id="71f4176ab9692bdc39092679646c812b11c8ebc89a93e8b10a39dafed2ca5eb1" state="working">
 <title>Regression on 1st dependencies download from bootstrap</title>
 <reporter>devaux</reporter>
 <creation>2023-02-24 13:53:23+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">bfca44f877c9cb6e2fbd92dbec6ba421c47c889dc70d28b370b8aa9013a7f45e</link>
 </links>
-<cf v="null"/>
+<cf v="devaux 2023-02-24T13:54:06+01:00"/>
 </cr>
