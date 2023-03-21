66d823b89993d540c7927a85b951b2f586523ec9
Nicolas FRAYSSE <nicolas.fraysse.external@ariane.group>
2023-03-21 16:49:50 +0100

    #598186f Added support for USEMOD, crate type and edition

--- _charm/src/598186fd09b61f87c633baa3b9254345b3f55d4aa98a1d2f79e7d66033fed0ff.cr
+++ _charm/src/598186fd09b61f87c633baa3b9254345b3f55d4aa98a1d2f79e7d66033fed0ff.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="598186fd09b61f87c633baa3b9254345b3f55d4aa98a1d2f79e7d66033fed0ff" state="open">
+<cr id="598186fd09b61f87c633baa3b9254345b3f55d4aa98a1d2f79e7d66033fed0ff" state="working">
 <title>Improve rust module</title>
 <reporter>ft30997</reporter>
 <creation>2023-03-21 16:46:59+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="null"/>
+<cf v="ft30997 2023-03-21T16:49:50+01:00"/>
 </cr>
--- core/module-rust.mk
+++ core/module-rust.mk
@@ -1,4 +1,4 @@
-$(info DDDDD rust)
+$(info Rust module detected)
 
 
 ifeq ($(ISWINDOWS),true)
@@ -7,11 +7,30 @@ else
 SOEXT=.so
 endif
 
+# Initialize name of rustc entry file
+ifeq ($(ENTRYFILENAME),)
+ENTRYFILENAME=lib
+endif
+
+# Initialize type of crate
+ifeq ($(CRATETYPE),)
+CRATETYPE=bin
+endif
+
+# Initialize rustc edition
+ifeq ($(EDITION),)
+EDITION=2018
+endif
+
 ifeq ($(CRATETYPE),dylib)
 TARGETDIR:=$(TRDIR)/lib
 TARGET=lib$(APPNAME)_$(MODNAME)$(SOEXT)
+else ifeq ($(CRATETYPE),rlib)
+TARGETDIR:=$(TRDIR)/lib
+TARGET=lib$(APPNAME)_$(MODNAME).rlib
 else
-TARGETDIR=:=$(TRDIR)/bin
+ENTRYFILENAME=main
+TARGETDIR:=$(TRDIR)/bin
 TARGET=$(APPNAME)_$(MODNAME)
 endif
 
@@ -21,6 +40,12 @@ RUSTSRCFILES:=$(filter %.rs,$(SRCFILES))
 
 RUSTC=rustc
 
+RUSTLIBDIR=$(TRDIR)/lib
+
+RUSTLIBS=$(foreach MOD,$(USEMOD),--extern $(MOD)=$(RUSTLIBDIR)/lib$(APPNAME)_$(MOD).rlib)
+
+RUSTFLAGS+=-L dependency=$(RUSTLIBDIR) $(RUSTLIBS)
+
 ifeq ($(MODE),debug)
 RUSTFLAGS+=-g
 endif
@@ -28,14 +53,10 @@ ifeq ($(MODE),release)
 RUSTFLAGS+=-O
 endif
 
-
-$(info DDDDD $(TARGETFILE))
-
-
 $(TARGETFILE): $(RUSTSRCFILES)
-	@$(ABS_PRINT_info) "Rust compile from src/$(APPNAME).rs"
+	@$(ABS_PRINT_info) "Rust compile $(CRATETYPE) from src/$(ENTRYFILENAME).rs"
 	@mkdir -p $(@D)
-	@$(RUSTC) --crate-type $(CRATETYPE) $(RUSTFLAGS) src/$(APPNAME).rs -o $@ && \
+	@$(RUSTC) --edition=$(EDITION) --crate-type $(CRATETYPE) $(RUSTFLAGS) src/$(ENTRYFILENAME).rs -o $@ && \
         $(ABS_PRINT_info) "Rust crate built: $@"
 
 all-impl:: $(TARGETFILE) 
