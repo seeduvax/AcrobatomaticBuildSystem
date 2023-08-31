feb90cad26538bd848c6357f9d31752239001c30
Sebastien Devaux <sebastien.devaux@eduvax.net>
2023-08-31 11:14:11 +0200

    #7bc74cc CR management

--- _charm/src/477105080934c3f98f125ee039250a60dd8ba44d511a2c7ba3417adc51f4f4a2.cr
+++ _charm/src/477105080934c3f98f125ee039250a60dd8ba44d511a2c7ba3417adc51f4f4a2.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="477105080934c3f98f125ee039250a60dd8ba44d511a2c7ba3417adc51f4f4a2" state="working">
+<cr id="477105080934c3f98f125ee039250a60dd8ba44d511a2c7ba3417adc51f4f4a2" state="resolved">
 <title>Styling for requirement management attributes</title>
 <reporter>m026258</reporter>
 <creation>2023-06-07 13:21:41+02:00</creation>
--- _charm/src/8a45331764fdb46c420fefbba3376ac1dde39e5961ad612bfa155d6b261af254.cr
+++ _charm/src/8a45331764fdb46c420fefbba3376ac1dde39e5961ad612bfa155d6b261af254.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="8a45331764fdb46c420fefbba3376ac1dde39e5961ad612bfa155d6b261af254" state="working">
+<cr id="8a45331764fdb46c420fefbba3376ac1dde39e5961ad612bfa155d6b261af254" state="resolved">
 <title>Rust language support</title>
 <reporter>sdevaux</reporter>
 <creation>2023-02-21 16:04:43+01:00</creation>
