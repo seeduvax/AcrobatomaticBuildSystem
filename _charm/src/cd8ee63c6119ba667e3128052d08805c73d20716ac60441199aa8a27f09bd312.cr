acc19803ea07e2399f1344a6c6952d3236e35778
Sebastien Devaux <sebastien.devaux@eduvax.net>
2023-10-11 13:51:01 +0200

    #7bc74cc issue management

---
+++ _charm/src/cd8ee63c6119ba667e3128052d08805c73d20716ac60441199aa8a27f09bd312.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="cd8ee63c6119ba667e3128052d08805c73d20716ac60441199aa8a27f09bd312" state="open">
+<title>ABS does not work inside a busybox shell</title>
+<reporter>sdevaux</reporter>
+<creation>2023-10-11 13:51:01+02:00</creation>
+<description></description>
+<links>
+<link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
+</links>
+<cf v="null"/>
+</cr>
