aaff8fb62b48dadea2a2a9044b8a17cd872f1f38
Sebastien Devaux <sebastien.devaux@eduvax.net>
2023-11-20 20:52:00 +0100

    #7bc74cc issue management

---
+++ _charm/src/45028ad6e7d02f959e9cb528afbfc57efc28f964300b9d1297895d342c6f31bb.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="45028ad6e7d02f959e9cb528afbfc57efc28f964300b9d1297895d342c6f31bb" state="open">
+<title>Trailing comma in clang db json shall be removed</title>
+<reporter>sdevaux</reporter>
+<creation>2023-11-20 20:52:00+01:00</creation>
+<description></description>
+<links>
+<link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
+</links>
+<cf v="null"/>
+</cr>
