a909721131c8b3832c1cf4ac4826e05325c3aded
Frédéric ASTRUC <astfred2@yahoo.fr>
2024-02-26 14:52:14 +0100

    #46a3ba8 Use tmp file to avoid corrupted files

--- _charm/src/46a3ba8b44f001e02e67b85781ead5efac45223ffc0e543ecfc1ab648960600b.cr
+++ _charm/src/46a3ba8b44f001e02e67b85781ead5efac45223ffc0e543ecfc1ab648960600b.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="46a3ba8b44f001e02e67b85781ead5efac45223ffc0e543ecfc1ab648960600b" state="open">
+<cr id="46a3ba8b44f001e02e67b85781ead5efac45223ffc0e543ecfc1ab648960600b" state="working">
 <title>It is possible to download corrupt files when stopping while downloading. Next make fail because of that and it is needed to do a cleanabs</title>
 <reporter>fred</reporter>
 <creation>2024-02-26 14:51:37+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="null"/>
+<cf v="m096258 2024-02-26T14:52:14+01:00"/>
 </cr>
