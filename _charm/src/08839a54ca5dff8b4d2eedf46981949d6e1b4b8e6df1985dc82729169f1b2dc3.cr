92f552b76ad4e120e12a2ab438344d9e5402755e
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-03-14 11:57:02 +0100

    #08839a5 added sol2 packaging

--- _charm/src/08839a54ca5dff8b4d2eedf46981949d6e1b4b8e6df1985dc82729169f1b2dc3.cr
+++ _charm/src/08839a54ca5dff8b4d2eedf46981949d6e1b4b8e6df1985dc82729169f1b2dc3.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="08839a54ca5dff8b4d2eedf46981949d6e1b4b8e6df1985dc82729169f1b2dc3" state="open">
+<cr id="08839a54ca5dff8b4d2eedf46981949d6e1b4b8e6df1985dc82729169f1b2dc3" state="working">
 <title>integrate sol2 as ABS package</title>
 <reporter>sdevaux</reporter>
 <creation>2024-03-14 11:56:28+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="null"/>
+<cf v="sdevaux 2024-03-14T11:57:15+01:00"/>
 </cr>
