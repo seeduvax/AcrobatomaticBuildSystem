37c26fae0d806cad811a3f04b7120bb8d903e911
laurentmldev <laurentmldev@github.com>
2024-06-06 11:29:22 +0200

    #7bc74cc issue management

---
+++ _charm/src/141834204b318d6344040c4ae3b517b0a40fb2f258a21f7ba8e5fbcae4ab8980.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="141834204b318d6344040c4ae3b517b0a40fb2f258a21f7ba8e5fbcae4ab8980" state="open">
+<title>Add pipe to HEML special symbols</title>
+<reporter>m026761</reporter>
+<creation>2024-06-06 11:29:22+02:00</creation>
+<description></description>
+<links>
+<link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
+</links>
+<cf v="null"/>
+</cr>
--- _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
+++ _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
@@ -26,6 +26,7 @@
 <link name="child">08839a54ca5dff8b4d2eedf46981949d6e1b4b8e6df1985dc82729169f1b2dc3</link>
 <link name="child">61d1a8945e8a8c7b8be920cc1da23264138f9bba454d4e7c3f1fed4591f7253e</link>
 <link name="child">ad41e022e1ac72e90ac05fff555cfcfe5c3784412ce0e4cd3fea265446fa85b8</link>
+<link name="child">141834204b318d6344040c4ae3b517b0a40fb2f258a21f7ba8e5fbcae4ab8980</link>
 </links>
 <cf v="sdevaux 2024-05-02T13:59:21+02:00"/>
 </cr>
