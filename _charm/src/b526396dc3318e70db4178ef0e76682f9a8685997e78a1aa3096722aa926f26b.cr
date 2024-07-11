2f40df069f3947ccf9646bc6093f4cb717799cee
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-07-11 14:37:18 +0200

    #7bc74cc issue management

--- _charm/src/b526396dc3318e70db4178ef0e76682f9a8685997e78a1aa3096722aa926f26b.cr
+++ _charm/src/b526396dc3318e70db4178ef0e76682f9a8685997e78a1aa3096722aa926f26b.cr
@@ -2,7 +2,7 @@
 <cr id="b526396dc3318e70db4178ef0e76682f9a8685997e78a1aa3096722aa926f26b" state="open">
 <title>hemldoc: generic item with identifier</title>
 <reporter>sdevaux</reporter>
-<creation>2024-07-11 14:35:42+02:00</creation>
+<creation>2024-07-11 14:37:18+02:00</creation>
 <description></description>
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
