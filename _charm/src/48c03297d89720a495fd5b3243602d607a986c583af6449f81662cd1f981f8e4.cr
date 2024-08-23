21efe8ff3a6ee7c34f07f03c6b8bdf35384f3ed2
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-08-23 13:37:04 +0200

    #7bc74cc issue management

---
+++ _charm/src/48c03297d89720a495fd5b3243602d607a986c583af6449f81662cd1f981f8e4.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="48c03297d89720a495fd5b3243602d607a986c583af6449f81662cd1f981f8e4" state="open">
+<title>No mor use find command</title>
+<reporter>m026258</reporter>
+<creation>2024-08-23 13:37:04+02:00</creation>
+<description></description>
+<links>
+<link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
+</links>
+<cf v="null"/>
+</cr>
