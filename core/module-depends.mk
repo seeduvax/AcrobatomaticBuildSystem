83fc307cdc32fa3168d4d7016191559a96c5c740
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-09-18 13:59:23 +0200

    #dae207a debuggin with real world project

--- core/module-depends.mk
+++ core/module-depends.mk
@@ -3,12 +3,15 @@ include module.cfg
 
 MODNAME?=$(notdir $(abspath .))
 
+
 $(PRJOBJDIR)/$(MODNAME)/moddeps.mk:
 	@$(ABS_PRINT_info) "Generating $(MODNAME) module dependency file."
 	@mkdir -p $(@D)
 	@echo "# "`date` > $@
 	@printf "\n\
-"'ifeq ($$(filter $$(MODNAME),$$(ABS_INCLUDE_MODS)),)'"\n\
+"'ifeq ($$(_module_$(APPNAME)_$(MODNAME)_dir),)'"\n\
+include $(patsubst %,$(PRJOBJDIR)/%/moddeps.mk,$(USEMOD))\n\
+\n\
 ABS_INCLUDE_MODS+=$(MODNAME)\n\
 _module_$(MODNAME)_dir=$(PRJROOT)/$(MODNAME)\n\
 _module_$(APPNAME)_$(MODNAME)_dir=$(TRDIR)\n\
