29388dcc186938ac54225a5044241a65ca963268
Fred <astfred2@yahoo.fr>
2024-09-25 08:49:44 +0200

    #7bc74cc issue management

---
+++ _charm/src/51648b00992afdb4cf39d3374d071e3af6685ddb39eaaa8708816611d9e5fe55.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="51648b00992afdb4cf39d3374d071e3af6685ddb39eaaa8708816611d9e5fe55" state="open">
+<title>Support code section in ABS_TEST_CASE. This will permit to the clang format to better reformate the code</title>
+<reporter>fred</reporter>
+<creation>2024-09-25 08:49:44+02:00</creation>
+<description></description>
+<links>
+<link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
+</links>
+<cf v="null"/>
+</cr>
