14d60b14ee6f5557a4b297bf0e217a36d7edc65d
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-10-07 13:52:43 +0200

    #dae207a a62c68c [tag switch] depedency refactoring for user project testing

--- _charm/src/dae207a7538ff30aec99037cec6b2c0ad6857a50d0e0df70aaf0d165eaae5024.cr
+++ _charm/src/dae207a7538ff30aec99037cec6b2c0ad6857a50d0e0df70aaf0d165eaae5024.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="m026258 2024-10-02T13:43:20+02:00"/>
+<cf v="m026258 2024-10-07T13:52:43+02:00"/>
 </cr>
