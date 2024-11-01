37e821528b14455206383b4f98699ce69d23b8f5
Sebastien Devaux <sebastien.devaux@eduvax.net>
2024-11-01 11:57:05 +0100

    #51648b0 small cppunit template improvement

--- core/module-test.mk
+++ core/module-test.mk
@@ -353,7 +353,7 @@ using namespace $(TNAMESPACE);\n\
 \n\
 // ----------------------------------------------------------\n\
 // test suite implementation\n\
-ABS_TEST_SUITE_BEGIN( Test$(TESTNAME) )\n\
+ABS_TEST_SUITE_BEGIN( $(TESTNAME) )\n\
 // uncomment and cmplete next line for test suite description\n\
 // ABS_TEST_DESCR(test description)\n\
 \n\
