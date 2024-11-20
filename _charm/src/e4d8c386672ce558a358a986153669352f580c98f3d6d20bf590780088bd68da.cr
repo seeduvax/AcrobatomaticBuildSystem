e531760f225190c4978a7806529fe041620d52c4
Frederic ASTRUC <astfred2@yahoo.fr>
2024-11-20 13:58:16 +0100

    #e4d8c38 add the check of errors

--- _charm/src/e4d8c386672ce558a358a986153669352f580c98f3d6d20bf590780088bd68da.cr
+++ _charm/src/e4d8c386672ce558a358a986153669352f580c98f3d6d20bf590780088bd68da.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="e4d8c386672ce558a358a986153669352f580c98f3d6d20bf590780088bd68da" state="open">
+<cr id="e4d8c386672ce558a358a986153669352f580c98f3d6d20bf590780088bd68da" state="working">
 <title>Add gitlab-ci yaml file</title>
 <reporter>m096258</reporter>
 <creation>2024-11-20 13:57:43+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="null"/>
+<cf v="m096258 2024-11-20T13:58:16+01:00"/>
 </cr>
