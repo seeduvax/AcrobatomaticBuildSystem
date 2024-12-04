745560f70dcfd39be31b0a7679946ad6fc909f6a
flohgh <flo@flo.flo>
2024-12-04 11:34:29 +0100

    #7bc74cc issue management

---
+++ _charm/src/c432eebd7a7b2557c2a263f6c66208b37a8c4ebba6f609f7aa75a61ffe83182b.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="c432eebd7a7b2557c2a263f6c66208b37a8c4ebba6f609f7aa75a61ffe83182b" state="open">
+<title>Add test obj files in compilee_commands.json</title>
+<reporter>m025909</reporter>
+<creation>2024-12-04 11:34:29+01:00</creation>
+<description></description>
+<links>
+<link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
+</links>
+<cf v="null"/>
+</cr>
