e69cbeca4c3f8907936cda39e1ae30ecb2ef6c80
Sebastien Devaux <sebastien.devaux@eduvax.net>
2025-02-17 22:25:45 +0100

    #7bc74cc issue management

--- _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
+++ _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
@@ -38,6 +38,7 @@
 <link name="child">9cdaa6851bed9b2a325115a637958f85e9f3f3e0e2a20ebf95dc116c923ae7bb</link>
 <link name="child">e4d8c386672ce558a358a986153669352f580c98f3d6d20bf590780088bd68da</link>
 <link name="child">c432eebd7a7b2557c2a263f6c66208b37a8c4ebba6f609f7aa75a61ffe83182b</link>
+<link name="child">d54bc0c8c8e3e90dc7a4e887cbf8f55f59715f89761368b868730c3ac06ec7e5</link>
 </links>
 <cf v="m026258 2024-12-02T14:22:43+01:00"/>
 </cr>
