468dc95a810dbdc0e7bef7bafc56c02910d58f60
fred322 <astfred2@yahoo.fr>
2025-03-12 18:52:37 +0100

    #e01499a Remove useless variables

--- _charm/src/e01499af7ccefbbce7893af1212765f2d04a659dc9c913c8177d06d232c1b606.cr
+++ _charm/src/e01499af7ccefbbce7893af1212765f2d04a659dc9c913c8177d06d232c1b606.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="m096258 2025-03-12T17:23:00+00:00"/>
+<cf v="fred322 2025-03-12T18:52:37+01:00"/>
 </cr>
--- core/module-crules-patches.mk
+++ core/module-crules-patches.mk
@@ -1,9 +1,5 @@
-PATCHED_SRC_DIR?=$(ARCHSRC_SRC_DIRECTORY)
-PATCHED_OBJ_TYPE?=.o
-
 ifneq ($(wildcard patches),)
 ALL_PATCHES=$(shell find patches/ -type f)
-
 ALL_PATCHED=$(patsubst patches/%.patch,$(OBJDIR)/%.patched,$(ALL_PATCHES))
 
 $(OBJS): $(ALL_PATCHED)
@@ -11,19 +7,19 @@ $(OBJS): $(ALL_PATCHED)
 $(OBJDIR)/%.patched: patches/%.patch $(ARCHSRC_INCLUDE_DIRECTORY)/%
 	@$(ABS_PRINT_info) "Patching $*"
 	@mkdir -p $(@D)
-	patch -p0 -d $(TRDIR) < $<
+	@patch -p0 -d $(TRDIR) < $<
 	@touch $@
 
 $(OBJDIR)/%.patched: patches/%.patch $(ARCHSRC_SRC_DIRECTORY)/%
 	@$(ABS_PRINT_info) "Patching $*"
 	@mkdir -p $(@D)
-	patch -p0 -d $(EXT_SRC_DIR) < $<
+	@patch -p0 -d $(EXT_SRC_DIR) < $<
 	@touch $@
 
 $(OBJDIR)/%.patched: patches/%.patch src/%
 	@$(ABS_PRINT_info) "Patching $*"
 	@mkdir -p $(@D)
-	patch -p0 -d $(EXT_SRC_DIR) < $<
+	@patch -p0 -d . < $<
 	@touch $@
 
 endif
