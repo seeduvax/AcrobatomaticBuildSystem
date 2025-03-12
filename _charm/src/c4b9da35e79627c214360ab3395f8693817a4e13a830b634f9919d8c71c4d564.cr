5bb11497ed64bc364d8edec7c669eaa246c38dd5
fred322 <astfred2@yahoo.fr>
2025-03-12 18:50:19 +0100

    #c4b9da3 Use git username if possible for charm tickets

--- _charm/src/c4b9da35e79627c214360ab3395f8693817a4e13a830b634f9919d8c71c4d564.cr
+++ _charm/src/c4b9da35e79627c214360ab3395f8693817a4e13a830b634f9919d8c71c4d564.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="c4b9da35e79627c214360ab3395f8693817a4e13a830b634f9919d8c71c4d564" state="open">
+<cr id="c4b9da35e79627c214360ab3395f8693817a4e13a830b634f9919d8c71c4d564" state="working">
 <title>Use git username for charm tickets</title>
 <reporter>fred</reporter>
 <creation>2025-03-12 18:49:53+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="null"/>
+<cf v="fred322 2025-03-12T18:50:19+01:00"/>
 </cr>
