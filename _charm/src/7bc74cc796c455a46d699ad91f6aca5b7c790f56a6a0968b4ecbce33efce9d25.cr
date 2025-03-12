a13f3bc1633bf0de01728fbd248e427e68dc5d4c
fred322 <astfred2@yahoo.fr>
2025-03-12 18:49:53 +0100

    #7bc74cc issue management

--- _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
+++ _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
@@ -47,6 +47,7 @@
 <link name="child">451ae92fcec7f48116c6e901fc5b5ecfeb24e3f84a3cc40a22b0be27366dd592</link>
 <link name="child">6a405ed59efe577e20a334097794bb8aafe8ae5affee8f8c3c96eb9124600307</link>
 <link name="child">e01499af7ccefbbce7893af1212765f2d04a659dc9c913c8177d06d232c1b606</link>
+<link name="child">c4b9da35e79627c214360ab3395f8693817a4e13a830b634f9919d8c71c4d564</link>
 </links>
 <cf v="m096258 2025-03-12T10:48:15+01:00"/>
 </cr>
