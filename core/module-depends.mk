38a11ff4278613510ca3a47c851f1b5ac6d5408a
fred322 <astfred2@yahoo.fr>
2025-03-16 12:54:07 +0100

    #dae207a FIx mods inclusiona and link

--- core/module-depends.mk
+++ core/module-depends.mk
@@ -10,13 +10,14 @@ MODNAME?=$(notdir $(abspath .))
 # .done must depends of other .done to propagate the multithread dependency.
 # the variable _depends will contains all libs needed by the module. If the archive name is same than .so, the name lib$(dep) will be used instead of $(dep)
 # The externals libs EXTLIBMAKES must be done before launching compilation of dependences.
+# ABS_INCLUDE_MODS must not include test libs or test mods
 $(PRJOBJDIR)/$(MODNAME)/moddeps.mk:
 	@$(ABS_PRINT_info) "Generating $(MODNAME) module dependency file."
 	@mkdir -p $(@D)
 	@echo "# "`date` > $@.tmp
 	@printf 'ifeq ($$(_module_$(APPNAME)_$(MODNAME)_dir),)\n'\
 'include $$(patsubst %%,$$(PRJOBJDIR)/%%/moddeps.mk,$(USEMOD))\n\n'\
-'ABS_INCLUDE_MODS+=$$(patsubst %%,$(APPNAME)_%%,$(USEMOD) $(TESTUSEMOD))\n'\
+'ABS_INCLUDE_MODS+=$$(patsubst %%,$(APPNAME)_%%,$(USEMOD))\n'\
 '_module_$(MODNAME)_dir=$$(PRJROOT)/$(MODNAME)\n'\
 '_module_$(APPNAME)_$(MODNAME)_dir=$$(TRDIR)\n'\
 '_module_$(APPNAME)_$(MODNAME)_depends_raw=$$(sort $(LINKLIB) $(INCLUDE_MODS) $(patsubst %,$(APPNAME)_%,$(USEMOD)) $(foreach dep,$(LINKLIB) $(INCLUDE_MODS) $(patsubst %,$(APPNAME)_%,$(USEMOD)),$$(_module_$(dep)_depends)))\n'\
