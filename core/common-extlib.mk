3b39da50774357e158de00b7e1d3fd9313810652
fred322 <astfred2@yahoo.fr>
2025-03-17 10:20:56 +0100

    #7bc74cc Add macros for checkdep targets

--- core/common-extlib.mk
+++ core/common-extlib.mk
@@ -463,32 +463,33 @@ endif
 ##  - checkdeptest: show currently defined dependencies including test (full graph including
 ##    dependencies of dependencies).
 ifneq ($(USELIB),)
-$(BUILDROOT)/$(APPNAME)_deps.dot: $(PRJROOT)/app.cfg
+define generateCheckDep
 	@$(ABS_PRINT_info) "Generating project dependency graph."
 	@printf 'digraph deps {\ngraph [rankdir="LR",ranksep=1];\nnode [width=2, shape=box, style="rounded"];\n' > $@
-	@printf ' $(foreach dep,$(USELIB),"$(APPNAME)-$(VERSION)"->"$(dep)";\n)' | sed -e 's/d";$$/d"\[color="orange"\];/g'>> $@
-	@printf ' $(foreach dep,$(ADDEDDEPLIST),$(dep);\n)' | sort -u | sed -e 's/d";$$/d"\[color="orange"\];/g'>> $@
+	@printf ' $(foreach dep,$(subst |,-,$1),"$(APPNAME)-$(VERSION)"->"$(dep)";\n)' | sed -e 's/d";$$/d"\[color="orange"\];/g'>> $@
+	@printf ' $(foreach dep,$(subst |,-,$(ADDEDDEPLIST)),$(dep);\n)' | sort -u | sed -e 's/d";$$/d"\[color="orange"\];/g'>> $@
 	@echo "}" >> $@
 	@dot -Tpng $@ > $@.png
-	
-$(BUILDROOT)/$(APPNAME)_testdeps.dot: $(PRJROOT)/app.cfg
-	@$(ABS_PRINT_info) "Generating project dependency graph."
-	@printf 'digraph deps {\ngraph [rankdir="LR",ranksep=1];\nnode [width=2, shape=box, style="rounded"];\n' > $@
-	@printf ' $(foreach dep,$(USELIB) $(TUSELIB),"$(APPNAME)-$(VERSION)"->"$(dep)";\n)' | sed -e 's/d";$$/d"\[color="orange"\];/g'>> $@
-	@printf ' $(foreach dep,$(ADDEDDEPLIST),$(dep);\n)' | sort -u | sed -e 's/d";$$/d"\[color="orange"\];/g'>> $@
-	@echo "}" >> $@
-	@dot -Tpng $@ > $@.png
+endef
 
-checkdep: $(BUILDROOT)/$(APPNAME)_deps.dot
+define showCheckDep
 	@$(ABS_PRINT_info) "Launching image viewer to display the generated dependency graph $<.png."
 	@$(ABS_PRINT_info) "Close image viewer to continue or hit Ctrl-C to stop here."
 	@xdot $< 2>/dev/null || eog $<.png 2>/dev/null || xdg-open $<.png 2>/dev/null || $(ABS_PRINT_error) "No image viewer found (expected one of: xdot, eog, xdg-open)"
+endef
+
+$(BUILDROOT)/$(APPNAME)_deps.dot: $(PRJROOT)/app.cfg
+	@$(call generateCheckDep,$(USELIB) $(TUSELIB))
+	
+$(BUILDROOT)/$(APPNAME)_testdeps.dot: $(PRJROOT)/app.cfg
+	@$(call generateCheckDep,$(USELIB))
+
+checkdep: $(BUILDROOT)/$(APPNAME)_deps.dot
+	@$(call showCheckDep)
 	
 
 checkdeptest: $(BUILDROOT)/$(APPNAME)_testdeps.dot
-	@$(ABS_PRINT_info) "Launching image viewer to display the generated dependency graph $<.png."
-	@$(ABS_PRINT_info) "Close image viewer to continue or hit Ctrl-C to stop here."
-	@xdot $< 2>/dev/null || eog $<.png 2>/dev/null || xdg-open $<.png 2>/dev/null || $(ABS_PRINT_error) "No image viewer found (expected one of: xdot, eog, xdg-open)"
+	@$(call showCheckDep)
 
 else #ifneq ($(USELIB),)
 checkdep:
