d12fd6383a82751dcde88acdd5fe873a315500c8
fred322 <astfred2@yahoo.fr>
2025-03-25 10:11:46 +0100

    #7bc74cc Fix double arrow in checkdep graph. Add checkmodsdep rule for fun

--- core/common-extlib.mk
+++ core/common-extlib.mk
@@ -466,14 +466,28 @@ endif # ifneq ($(MAKECMDGOALS),checkdep%)
 ## Targets:
 ##  - checkdep: show currently defined dependencies (full graph including
 ##    dependencies of dependencies).
+##  - checkmodsdep: show currently defined modules dependencies (full graph including
+##    dependencies of dependencies of modules).
 ##  - checkdeptest: show currently defined dependencies including test (full graph including
 ##    dependencies of dependencies).
 ifneq ($(USELIB),)
 define generateCheckDep
 	@$(ABS_PRINT_info) "Generating project dependency graph."
 	@printf 'digraph deps {\ngraph [rankdir="LR",ranksep=1];\nnode [width=2, shape=box, style="rounded"];\n' > $@
-	@printf ' $(foreach dep,$(subst |,-,$1),"$(APPNAME)-$(VERSION)"->"$(dep)";\n)' | sed -e 's/d";$$/d"\[color="orange"\];/g'>> $@
+	@printf ' $(foreach dep,$(subst |,-,$(sort $1)),"$(APPNAME)-$(VERSION)"->"$(dep)";\n)' | sed -e 's/d";$$/d"\[color="orange"\];/g'>> $@
 	@printf ' $(foreach dep,$(subst |,-,$(ADDEDDEPLIST)),$(dep);\n)' | sort -u | sed -e 's/d";$$/d"\[color="orange"\];/g'>> $@
+	@echo "}" >> $@
+	@dot -Tpng $@ > $@.png
+endef
+
+define getModsDepsForGraph
+$(if $(filter $1,$2),,$(foreach depend,$(_module_$1_depends),"$1"->"$(depend)";\n $(call getModsDepsForGraph,$(depend),$2 $1)))
+endef
+define generateCheckModDep
+	@$(ABS_PRINT_info) "Generating modules dependency graph."
+	@printf 'digraph deps {\ngraph [rankdir="LR",ranksep=1];\nnode [width=2, shape=box, style="rounded"];\n' > $@
+	@printf ' $(foreach dep,$(subst |,-,$(sort $1)),"$(APPNAME)"->"$(APPNAME)_$(dep)";\n)'>> $@
+	@printf ' $(sort $(foreach dep,$(subst |,-,$1),$(call getModsDepsForGraph,$(APPNAME)_$(dep))))' | sort -u>> $@
 	@echo "}" >> $@
 	@dot -Tpng $@ > $@.png
 endef
@@ -490,7 +504,13 @@ $(BUILDROOT)/$(APPNAME)_deps.dot: $(PRJROOT)/app.cfg
 $(BUILDROOT)/$(APPNAME)_testdeps.dot: $(PRJROOT)/app.cfg
 	@$(call generateCheckDep,$(USELIB) $(NDUSELIB))
 
+$(BUILDROOT)/$(APPNAME)_modsdeps.dot: $(PRJROOT)/app.cfg
+	@$(call generateCheckModDep,$(MODULES))
+
 checkdep: $(BUILDROOT)/$(APPNAME)_deps.dot
+	@$(call showCheckDep)
+
+checkmodsdep: $(BUILDROOT)/$(APPNAME)_modsdeps.dot
 	@$(call showCheckDep)
 
 checkdeptest: $(BUILDROOT)/$(APPNAME)_testdeps.dot
