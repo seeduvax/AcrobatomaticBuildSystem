15dcc004ce7eb0bc789af85823f0455b2989785e
fred322 <astfred2@yahoo.fr>
2025-04-02 17:26:43 +0200

    #dae207a Fix test

--- sampleprj/T_abs/test/scripts/testTests.sh
+++ sampleprj/T_abs/test/scripts/testTests.sh
@@ -8,7 +8,7 @@ testLinked projA cppexe projA_cpplib
 executeMake testbuild projB
 testLinkedInBuild projB cpplib projA_cpplib test
 testLinkedInBuild projB cpplib2 projA_cpplib testlib-dashed
-testLinkedInBuild projB t_cpplib2 cppunit projB_cpplib2 projB_cpplib3
+testLinkedInBuild projB t_cpplib2 cppunit projA_cpplib projB_cpplib2 projB_cpplib3 testlib-dashed
 
 testFile $MODROOT/test/resources/expected/TestExample.h $testDirectory/projB/build/NotALinux/$MODE/obj/cpplib2/test
 
