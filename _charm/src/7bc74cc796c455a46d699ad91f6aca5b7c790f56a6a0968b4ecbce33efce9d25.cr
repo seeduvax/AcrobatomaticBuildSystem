483aef62abe0659e2871fe71f1a10d164b193a7d
fred322 <astfred2@yahoo.fr>
2025-04-18 09:42:38 +0200

    #7bc74cc issue management

--- _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
+++ _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
@@ -49,6 +49,7 @@
 <link name="child">e01499af7ccefbbce7893af1212765f2d04a659dc9c913c8177d06d232c1b606</link>
 <link name="child">c4b9da35e79627c214360ab3395f8693817a4e13a830b634f9919d8c71c4d564</link>
 <link name="child">e5630a2f84efbb6bffc071fe831cf96b45e6d8de15c4221b13f1290f87e51972</link>
+<link name="child">8a29fde84195faf6423567823a78b3f14ee8b2ca677a94ee534e276a5c693336</link>
 </links>
 <cf v="fred322 2025-04-17T18:07:56+02:00"/>
 </cr>
---
+++ _charm/src/8a29fde84195faf6423567823a78b3f14ee8b2ca677a94ee534e276a5c693336.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="8a29fde84195faf6423567823a78b3f14ee8b2ca677a94ee534e276a5c693336" state="open">
+<title>Simplify targets files handle</title>
+<reporter>fred322</reporter>
+<creation>2025-04-18 09:42:38+02:00</creation>
+<description></description>
+<links>
+<link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
+</links>
+<cf v="null"/>
+</cr>
