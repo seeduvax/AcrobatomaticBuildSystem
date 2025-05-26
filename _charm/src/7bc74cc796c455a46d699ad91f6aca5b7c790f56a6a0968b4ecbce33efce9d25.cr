9090d7332f980ec9397c7631ead488ec2bf37b6f
Florian HARADJI <florian.haradji@ariane.group>
2025-05-26 15:14:22 +0200

    #7bc74cc issue management

--- _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
+++ _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
@@ -52,6 +52,7 @@
 <link name="child">8a29fde84195faf6423567823a78b3f14ee8b2ca677a94ee534e276a5c693336</link>
 <link name="child">20c720dd74608005dbd446665650cf173bb7616d58b34792d0b6acee385b3534</link>
 <link name="child">165cc97ed82df999c73e21abdebcca3499d8c1775f1dc378244a2ae4619d6e09</link>
+<link name="child">36a200b34258e5f3529ddf0f86ff4a6d137887e6104359a6f5871a4c604dbeb3</link>
 </links>
 <cf v="fred322 2025-04-30T14:45:57+02:00"/>
 </cr>
