9bb90415c509441c90fa14770e594a833f6d0944
Sebastien Devaux <sebastien.devaux@ariane.group>
2025-07-02 13:38:33 +0200

    #b6ce087 restoring {ext} without 1s dot

--- _charm/src/b6ce0872ce6312705d7b7dda4d3f6ead0a903d8106eccab76a0c4c16380779dd.cr
+++ _charm/src/b6ce0872ce6312705d7b7dda4d3f6ead0a903d8106eccab76a0c4c16380779dd.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="fred322 2025-07-02T10:53:27+02:00"/>
+<cf v="Sebastien Devaux 2025-07-02T13:39:05+02:00"/>
 </cr>
--- core/common-extlib.mk
+++ core/common-extlib.mk
@@ -117,7 +117,7 @@ ABS_DEPDOWNLOAD_RULE_OVERLOADED:=1
 .PRECIOUS: $(ABSWS_EXTLIBDIR)/%/import.mk $(ABSWS_NDEXTLIBDIR)/%/import.mk $(ABSWS_NA_EXTLIBDIR)/%/import.mk $(ABSWS_NDNA_EXTLIBDIR)/%/import.mk
 
 OPEN_BRACE:={
-ABS_REPO_TEMPLATE=$(foreach entry, $(ABS_REPO),$(if $(findstring $(OPEN_BRACE),$(entry)),$(entry),$(entry)/{arch}/{name}-{version}.{arch}{ext} $(entry)/{arch}/{name}/{name}-{version}.{arch}{ext} $(entry)/noarch/{name}-{version}{ext}))
+ABS_REPO_TEMPLATE=$(foreach entry, $(ABS_REPO),$(if $(findstring $(OPEN_BRACE),$(entry)),$(entry),$(entry)/{arch}/{name}-{version}.{arch}.{ext} $(entry)/{arch}/{name}/{name}-{version}.{arch}.{ext} $(entry)/noarch/{name}-{version}.{ext}))
 
 # fetch package with wget (any URL kind that wget can handle)
 # Caution: empty line at end of macro def is required for proper commands
@@ -186,12 +186,12 @@ endef
 # Get URL list related to a package
 # $1 package file name. expected format is <name>/<version>/pck.<ext>
 define GetDownloadURLs
-$(call SubstituteRepoTemplate,$(word 1,$(subst /, ,$1)),$(word 2,$(subst /, ,$1)),$(ARCH),$(subst pck,,$(word 3,$(subst /, ,$1)))) \
-$(call SubstituteRepoTemplate,$(word 1,$(subst /, ,$1)),$(word 2,$(subst /, ,$1)),noarch,$(subst pck,,$(word 3,$(subst /, ,$1))))
+$(call SubstituteRepoTemplate,$(word 1,$(subst /, ,$1)),$(word 2,$(subst /, ,$1)),$(ARCH),$(subst pck.,,$(word 3,$(subst /, ,$1))))
+$(call SubstituteRepoTemplate,$(word 1,$(subst /, ,$1)),$(word 2,$(subst /, ,$1)),noarch,$(subst pck.,,$(word 3,$(subst /, ,$1))))
 endef
 
 define GetNoarchDownloadURLs
-$(call SubstituteRepoTemplate,$(word 1,$(subst /, ,$1)),$(word 2,$(subst /, ,$1)),noarch,$(subst pck,,$(word 3,$(subst /, ,$1))))
+$(call SubstituteRepoTemplate,$(word 1,$(subst /, ,$1)),$(word 2,$(subst /, ,$1)),noarch,$(subst pck.,,$(word 3,$(subst /, ,$1))))
 endef
 
 # Get URL list related to a raw package package file name
