b6ca44bd9c52ebdaf73a14933d17b5a7b9900af2
fred322 <astfred2@yahoo.fr>
2025-12-09 10:04:24 +0100

    #0f0eafb add package fontenc T1 to have correctly render of _ character

--- _charm/src/0f0eafb62eb987be6f6f4d4af637c01ce401f7976df5c149dfb86500c58516a6.cr
+++ _charm/src/0f0eafb62eb987be6f6f4d4af637c01ce401f7976df5c149dfb86500c58516a6.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="0f0eafb62eb987be6f6f4d4af637c01ce401f7976df5c149dfb86500c58516a6" state="open">
+<cr id="0f0eafb62eb987be6f6f4d4af637c01ce401f7976df5c149dfb86500c58516a6" state="working">
 <title>The _ is render as a space in generated pdf by pdflatex</title>
 <reporter>fred322</reporter>
 <creation>2025-12-09 10:02:05+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="null"/>
+<cf v="fred322 2025-12-09T10:04:24+01:00"/>
 </cr>
