7412618d675a9986a0dca98c3ea2731bf4fdc5ee
fred322 <astfred2@yahoo.fr>
2025-12-10 11:56:43 +0100

    #1d430c7 add macro LoadModTypeMakefiles to load makefiles for given modtype

--- _charm/src/1d430c7e77db9ef8b76b793776cf1d087be6a4c05831903d4887e1b9bad2aeaf.cr
+++ _charm/src/1d430c7e77db9ef8b76b793776cf1d087be6a4c05831903d4887e1b9bad2aeaf.cr
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="1d430c7e77db9ef8b76b793776cf1d087be6a4c05831903d4887e1b9bad2aeaf" state="open">
-<title>Add macro to load makefiles associated to a modtype (callable from abs extensions=</title>
+<cr id="1d430c7e77db9ef8b76b793776cf1d087be6a4c05831903d4887e1b9bad2aeaf" state="working">
+<title>Add macro to load makefiles associated to a modtype (callable from abs extensions)</title>
 <reporter>fred322</reporter>
 <creation>2025-12-10 11:42:23+01:00</creation>
 <description></description>
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="null"/>
+<cf v="fred322 2025-12-10T11:56:43+01:00"/>
 </cr>
