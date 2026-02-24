9b74ff03b521021418e3735a4d67d7f1f3b77b35
Frederic ASTRUC <astfred2@yahoo.fr>
2026-02-24 08:31:40 +0100

    #3f388a9 Disable parametre propagation to make when executing dist to avoid parallele execution

--- _charm/src/3f388a9608d7678d78fee4d7bbb936381dadfc01624ca306ffcd7547590ec1b4.cr
+++ _charm/src/3f388a9608d7678d78fee4d7bbb936381dadfc01624ca306ffcd7547590ec1b4.cr
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="utf-8"?>
-<cr id="3f388a9608d7678d78fee4d7bbb936381dadfc01624ca306ffcd7547590ec1b4" state="open">
+<cr id="3f388a9608d7678d78fee4d7bbb936381dadfc01624ca306ffcd7547590ec1b4" state="working">
 <title>Files missing in module description when launching dist in parallele mode</title>
 <reporter>m096258</reporter>
 <creation>2026-02-24 08:31:10+01:00</creation>
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="null"/>
+<cf v="Frederic ASTRUC 2026-02-24T08:31:40+01:00"/>
 </cr>
