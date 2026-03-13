a68b7f534dd51ed7e96dd4caffdb9b3b1b569960
Frederic ASTRUC <astfred2@yahoo.fr>
2026-03-13 09:10:37 +0100

    #7bc74cc issue management

---
+++ _charm/src/757250baa7ed46212bef16599fb8f57defa92a56b744af5f78ce431f0a7add2f.cr
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="utf-8"?>
+<cr id="757250baa7ed46212bef16599fb8f57defa92a56b744af5f78ce431f0a7add2f" state="open">
+<title>Add trdir includes in vscodeconfig</title>
+<reporter>m096258</reporter>
+<creation>2026-03-13 09:10:37+01:00</creation>
+<description></description>
+<links>
+<link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
+</links>
+<cf v="null"/>
+</cr>
--- _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
+++ _charm/src/7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25.cr
@@ -71,6 +71,7 @@
 <link name="child">659e350a75d0136c00e6055cfc97673bd9a99992477c1dd4bb01ada1e39442fc</link>
 <link name="child">6dc6cb6f87e5ce07015db607ad0dd18ef7160ece987c2c67d38cda8d7bbec15e</link>
 <link name="child">3f388a9608d7678d78fee4d7bbb936381dadfc01624ca306ffcd7547590ec1b4</link>
+<link name="child">757250baa7ed46212bef16599fb8f57defa92a56b744af5f78ce431f0a7add2f</link>
 </links>
 <cf v="Frederic ASTRUC 2026-02-12T14:03:33+01:00"/>
 </cr>
