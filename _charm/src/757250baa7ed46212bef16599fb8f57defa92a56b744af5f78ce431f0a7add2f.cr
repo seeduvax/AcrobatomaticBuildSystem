e5c7f102167dda76434d384a0554097bb1589232
Frederic ASTRUC <astfred2@yahoo.fr>
2026-04-28 13:14:52 +0200

    #757250b Add includes from CFlags and CXXFLAGS in vscodeconfig

--- _charm/src/757250baa7ed46212bef16599fb8f57defa92a56b744af5f78ce431f0a7add2f.cr
+++ _charm/src/757250baa7ed46212bef16599fb8f57defa92a56b744af5f78ce431f0a7add2f.cr
@@ -7,5 +7,5 @@
 <links>
 <link name="parent">7bc74cc796c455a46d699ad91f6aca5b7c790f56a6a0968b4ecbce33efce9d25</link>
 </links>
-<cf v="Frederic ASTRUC 2026-03-13T09:11:08+01:00"/>
+<cf v="Frederic ASTRUC 2026-04-28T13:14:52+02:00"/>
 </cr>
--- core/app-vscode.mk
+++ core/app-vscode.mk
@@ -10,6 +10,17 @@ VSCODE_CPP_CONFIG=$(PRJROOT)/.vscode/c_cpp_properties.json
 vscodeconfig: $(VSCODE_CPP_CONFIG)
 
 MODULES_WITH_INCLUDES=$(foreach mod,$(MODULES),$(if $(wildcard $(mod)/include),$(mod)))
+VSCODE_PATHS_FROM_EXTLIBS=$(wildcard $(EXTLIBDIR)/*/*/include) $(wildcard $(NA_EXTLIBDIR)/*/*/include) $(wildcard $(NDEXTLIBDIR)/*/*/include) $(wildcard $(NDNA_EXTLIBDIR)/*/*/include)
+# USE CFLAGS et CXXFLAGS for includes defined in external libs
+VSCODE_PATHS_FROM_CFLAGS=$(foreach path,$(sort $(patsubst -I%,%,$(filter -I%,$(CXXFLAGS) $(CFLAGS)))),$(if $(wildcard $(path)),$(path)))
+
+VSCODE_ALL_INCLUDES=$(patsubst %,$(PRJROOT)/%/include/,$(MODULES_WITH_INCLUDES))
+VSCODE_ALL_INCLUDES+=$(TRDIR)/include/
+VSCODE_ALL_INCLUDES+=$(VSCODE_PATHS_FROM_EXTLIBS)
+VSCODE_ALL_INCLUDES+=$(VSCODE_PATHS_FROM_CFLAGS)
+
+VSCODE_ALL_INCLUDES_REL=$(patsubst $(PRJROOT)/%,%,$(foreach path,$(VSCODE_ALL_INCLUDES),$(if $(wildcard $(path)),$(path))))
+
 .PHONY: $(VSCODE_CPP_CONFIG)
 $(VSCODE_CPP_CONFIG): app.cfg
 	@$(ABS_PRINT_info) "Generation of $@"
@@ -19,9 +30,7 @@ $(VSCODE_CPP_CONFIG): app.cfg
 	@printf '    {\n' >> $@.tmp
 	@printf '        "name": "Linux",\n' >> $@.tmp
 	@printf '        "includePath": [\n' >> $@.tmp
-	@$(foreach mod,$(MODULES_WITH_INCLUDES),printf '            "$${workspaceFolder}/$(mod)/include/",\n' >> $@.tmp;)
-	@printf '            "$${workspaceFolder}/$(patsubst $(PRJROOT)/%,%,$(TRDIR))/include/",\n' >> $@.tmp
-	@$(foreach path,$(patsubst $(PRJROOT)/%,%,$(wildcard $(EXTLIBDIR)/*/*/include) $(wildcard $(NA_EXTLIBDIR)/*/*/include) $(wildcard $(NDEXTLIBDIR)/*/*/include) $(wildcard $(NDNA_EXTLIBDIR)/*/*/include)),printf '            "$${workspaceFolder}/$(path)",\n' >> $@.tmp;)
+	@$(foreach path,$(sort $(VSCODE_ALL_INCLUDES_REL)),printf '            "$${workspaceFolder}/$(path)",\n' >> $@.tmp;)
 	@printf '            "$(ABSROOT)/core/include/"\n' >> $@.tmp
 	@printf '        ],\n' >> $@.tmp
 	@$(if $(filter true,$(DISABLE_COMPILE_COMMANDS)),,printf '        "compileCommands": "$${workspaceFolder}/compile_commands.json"\n' >> $@.tmp)
